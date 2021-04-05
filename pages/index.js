5535f577d707685de5a6fc94d506b1667a2a5ae2
ilyandho <ii.ndhote@gmail.com>
2021-04-05 20:34:03 +0300

    setup for tailwind and Sacramental font for logo

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Sacramento&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+
+      <main>
+        <h1 className="font-sacramento font-black text-">Iit was brought</h1>
+      </main>
+    </div>
+  );
+}
